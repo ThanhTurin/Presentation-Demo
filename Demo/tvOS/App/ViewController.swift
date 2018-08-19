c627b004af914fb38fbf9bccc822920a8b7c4dc9
thanhturin <thanhturin@gmail.com>
2018-08-19 23:51:18 +0800

    Set up project

---
+++ Demo/tvOS/App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo-tvOS
+//
+//  Created by Thanh Turin on 8/19/18.
+//  Copyright © 2018 Thanh Turin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
